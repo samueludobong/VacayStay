0826024ec0d2d45780706a6a79ea733664ab5c07
Samuel Udobong <samuelleumasudo@gmail.com>
2025-11-24 23:01:03 +0100

    Modify User schema for optional fields
    Updated user schema to make image and recentSearchedCities optional with default values.

--- server/models/User.js
+++ server/models/User.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema(
   {
-    _id: { type: String, required: true },
+    _id: { type: String, required: true }, // Clerk ID
     username: { type: String, required: true },
     email: { type: String, required: true },
-    image: { type: String, required: true },
-    role: { type: String, enum: ["user", "hotelOwner"], default: "user", },
-    recentSearchedCities: [{ type: String, required: true }],
-  }, { timestamps: true }
+    image: { type: String, default: "" }, // optional
+    role: { type: String, enum: ["user", "hotelOwner"], default: "user" },
+    recentSearchedCities: { type: [String], default: [] }, // not required
+  },
+  { timestamps: true }
 );
 
 const User = mongoose.model("User", userSchema);
