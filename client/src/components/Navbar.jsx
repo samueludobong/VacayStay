2367cb47bc3ee8353d0278456a34c86f26143fe5
Samuel Udobong <samuelleumasudo@gmail.com>
2025-11-26 15:51:22 +0100

    Change Navbar background color on scroll

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
     }, [location.pathname]);
 
     return (
-        <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${isScrolled ? "bg-white/80 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4" : "py-4 md:py-6"}`}>
+        <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${isScrolled ? "bg-[#1e3b96] shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4" : "bg-[#1e3b96] py-4 md:py-6"}`}>
             <Link to="/">
                 <img src={assets.logo} alt="logo" className={`h-9 ${isScrolled && "invert opacity-80"}`} />
             </Link>
