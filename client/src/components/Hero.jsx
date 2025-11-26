dbd4306a03c6536e3384f7d2db68b900d0f526a3
Samuel Udobong <samuelleumasudo@gmail.com>
2025-11-26 16:46:47 +0100

    Center form and set max width in Hero component
    Updated form layout to center and limit maximum width.

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -65,7 +65,9 @@ const Hero = () => {
                 width="120" />
         </div>
             </main>
-           <form onSubmit={onSearch} className='bg-white text-gray-500 rounded-lg px-6 py-4 mt-8 flex flex-col md:flex-row max-md:items-center gap-4 max-md:mx-auto'>
+           <form onSubmit={onSearch} className="bg-white text-gray-500 rounded-lg px-6 py-4 mt-8 
+             flex flex-col md:flex-row max-md:items-center gap-4 
+             mx-auto justify-center max-w-5xl">
                 <div>
                     <div className='flex items-center gap-2'>
                         <img src={assets.calenderIcon} alt="" className='h-4' />
