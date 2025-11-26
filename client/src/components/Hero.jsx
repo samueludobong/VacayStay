9b8fba434c1d5b241e2b8ad59e4b0546bd65bc08
Samuel Udobong <samuelleumasudo@gmail.com>
2025-11-26 18:08:06 +0100

    Modify Hero section margin and form layout
    Updated the Hero component to adjust the section's margin.

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
     }
 
     return (
-      <section id="section" class="bg-[#0a1b43] mt-20">
+      <section id="section" class="bg-[#0a1b43] mt-20 mb-30">
     <main
         class="flex flex-col md:flex-row items-center max-md:text-center justify-between mt-16 pb-16 px-6 sm:px-10 md:px-24 max-w-7xl mx-auto w-full">
         <div class="flex flex-col items-center md:items-start">
@@ -69,7 +69,7 @@ const Hero = () => {
             </main>
            <form onSubmit={onSearch} className="bg-white text-black rounded-lg py-4 
              flex flex-col md:flex-row max-md:items-center gap-4 
-             mx-auto justify-center max-w-5xl mb-30">
+             mx-auto justify-center max-w-5xl">
                 <div>
                     <div className='flex items-center gap-2'>
                         <img src={assets.calenderIcon} alt="" className='h-4' />
