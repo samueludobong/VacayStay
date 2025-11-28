1b89e9d6e13200a5fb7a45cac38a12fce5626803
Samuel Udobong <samuelleumasudo@gmail.com>
2025-11-28 13:31:41 +0100

    update

--- client/src/components/what-we-do-section.jsx
+++ client/src/components/what-we-do-section.jsx
@@ -1,7 +1,8 @@
 
 export default function WhatWeDoSection() {
     return (
-        <section className="flex flex-col md:flex-row items-center justify-center gap-20 mt-20">
+        <section>
+        <div className="flex flex-col md:flex-row items-center justify-center gap-20 mt-20">
             <div className="relative shadow-2xl shadow-indigo-600/40 rounded-2xl overflow-hidden shrink-0">
                 <img className="max-w-sm w-full object-cover rounded-2xl"
                     src="https://images.unsplash.com/photo-1531497865144-0464ef8fb9a9?q=80&w=451&h=451&auto=format&fit=crop"
@@ -26,16 +27,55 @@ export default function WhatWeDoSection() {
             <div className="text-sm text-slate-600 max-w-md">
                 <h1 className="text-xl uppercase font-semibold text-slate-700">What we do?</h1>
                 <div className="w-24 h-[3px] rounded-full bg-linear-to-r from-indigo-600 to-[#DDD9FF]"></div>
-                <p className="mt-8">PrebuiltUI helps you build faster by transforming your design vision into fully functional,
-                    production-ready UI components. </p>
-                <p className="mt-4">Whether you're launching a SaaS app, landing page, or dashboard, our collection of Tailwind
-                    CSS components is crafted to boost your development speed and improve user experience.</p>
-                <p className="mt-4">From UI design systems to automation-ready layouts, PrebuiltUI empowers you to build
-                    beautifully and scale effortlessly.</p>
-                <button className="flex items-center gap-2 mt-8 hover:opacity-90 transition btn py-3 px-8 rounded-full text-white">
-                    <span>Read more</span>
-                </button>
+                <p className="mt-8">
+                VacayStay makes finding the perfect hotel effortless by turning your travel plans into smooth, stress-free stays.
+                </p>
+                <p className="mt-4">
+                Whether you're booking a luxury getaway, a family-friendly room, or a budget stay, our curated listings are designed to give you comfort, convenience, and a seamless booking experience.
+                </p>
+                <p className="mt-4">
+                From premium hotels to cozy apartments, VacayStay empowers you to discover the ideal stay, book instantly, and enjoy your trip your way.
+                </p>
             </div>
+            </div>
+            <div className="relative shadow-2xl shadow-indigo-600/40 rounded-2xl overflow-hidden shrink-0">
+            <div className="text-sm text-slate-600 max-w-md">
+                <h1 className="text-xl uppercase font-semibold text-slate-700">What we do?</h1>
+                <div className="w-24 h-[3px] rounded-full bg-linear-to-r from-indigo-600 to-[#DDD9FF]"></div>
+                <p className="mt-8">
+                VacayStay makes finding the perfect hotel effortless by turning your travel plans into smooth, stress-free stays.
+                </p>
+                <p className="mt-4">
+                Whether you're booking a luxury getaway, a family-friendly room, or a budget stay, our curated listings are designed to give you comfort, convenience, and a seamless booking experience.
+                </p>
+                <p className="mt-4">
+                From premium hotels to cozy apartments, VacayStay empowers you to discover the ideal stay, book instantly, and enjoy your trip your way.
+                </p>
+            </div>
+
+            <div className="relative shadow-2xl shadow-indigo-600/40 rounded-2xl overflow-hidden shrink-0">
+                <img className="max-w-sm w-full object-cover rounded-2xl"
+                    src="https://images.unsplash.com/photo-1531497865144-0464ef8fb9a9?q=80&w=451&h=451&auto=format&fit=crop"
+                    alt="" />
+                <div className="flex items-center gap-1 max-w-72 absolute bottom-8 left-8 bg-white p-4 rounded-xl">
+                    <div className="flex -space-x-4 shrink-0">
+                        <img src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200" alt="image"
+                            className="size-9 rounded-full border-[3px] border-white hover:-translate-y-1 transition z-1" />
+                        <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200" alt="image"
+                            className="size-9 rounded-full border-[3px] border-white hover:-translate-y-1 transition z-2" />
+                        <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?q=80&w=200&h=200&auto=format&fit=crop"
+                            alt="image"
+                            className="size-9 rounded-full border-[3px] border-white hover:-translate-y-1 transition z-3" />
+                        <div
+                            className="flex items-center justify-center text-xs  text-white size-9 rounded-full border-[3px] border-white bg-indigo-600 hover:-translate-y-1 transition z-4">
+                            50+
+                        </div>
+                    </div>
+                    <p className="text-sm font-medium text-slate-800">Join our developer community</p>
+                </div>
+                </div>
+                </div>
         </section>
+        
     );
 };
