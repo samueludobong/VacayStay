31b8dfdee5fab2373e86dd8a9c51a131e64a5c29
Samuel Udobong <samuelleumasudo@gmail.com>
2025-11-29 16:31:26 +0100

    update

--- client/src/pages/AllHotels.jsx
+++ client/src/pages/AllHotels.jsx
@@ -4,18 +4,27 @@ import Title from '../components/Title';
 import HotelCard from '../components/HotelCard';
 
 
-const FeaturedDestination = () => {
-    const { hotels, navigate } = useAppContext();
+const AllHotels = () => {
+    const { hotels } = useAppContext();
+
     return hotels.length > 0 && (
         <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
-            <Title title="Hotels" subTitle="Discover our handpicked selection of exceptional properties around the world, offering unparalleled luxury and unforgettable experiences." />
+            <Title 
+                title="Hotels" 
+                subTitle="Discover our handpicked selection of exceptional properties around the world, offering unparalleled luxury and unforgettable experiences." 
+            />
+
             <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
-                {rooms.slice(0, 4).map((room, index) => (
-                    <HotelCard key={hotels._id} room={room} index={index} />
+                {hotels.slice(0, 4).map((hotel, index) => (
+                    <HotelCard 
+                        key={hotel._id} 
+                        hotel={hotel} 
+                        index={index} 
+                    />
                 ))}
             </div>
         </div>
-    )
+    );
 }
 
-export default FeaturedDestination
+export default AllHotels;
