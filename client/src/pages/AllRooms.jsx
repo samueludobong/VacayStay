fe58362a1c6cb18c6b5f003a45148d1f17c8a913
Samuel Udobong <samuelleumasudo@gmail.com>
2025-11-30 19:54:50 +0100

    update

--- client/src/pages/AllRooms.jsx
+++ client/src/pages/AllRooms.jsx
@@ -123,9 +123,10 @@ const AllRooms = () => {
     return (
         <div className='flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24 xl:px-32'>
             <div>
-                {/* Main Title */}
                 <div className="flex flex-col items-start text-left">
-                    <h1 className='font-playfair text-4xl md:text-[40px]'>{filteredRooms.hotel.name} Hotel Rooms</h1>
+                <h1 className='font-playfair text-4xl md:text-[40px]'>
+                    {filteredRooms[0]?.hotel?.name} Hotel Rooms
+                </h1>
                     <p className='text-sm md:text-base text-gray-500/90 mt-2 max-w-174'>Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memories.</p>
                 </div>
 
