94e47143186d7d27b0b5fa229e8b3fc59224dd11
Samuel Udobong <samuelleumasudo@gmail.com>
2025-11-30 10:26:58 +0100

    fixes

--- client/src/pages/admin/ListHotels.jsx
+++ client/src/pages/admin/ListHotels.jsx
@@ -51,16 +51,18 @@ const ListRoom = () => {
 
     return (
         <div>
-            <Title align='left' font='outfit' title='Room Listings' subTitle='View, edit, or manage all listed rooms. Keep the information up-to-date to provide the best experience for users.' />
-            <p className='text-gray-500 mt-8'>Total Hotels</p>
+            <Title align='left' font='outfit' title='Hotel Listings' subTitle='View, edit, or manage all listed hotels. Keep the information up-to-date to provide the best experience for users.' />
+            <p className='text-gray-500 mt-8'>Pending Hotel Request</p>
             {/* Table with heads User Name, Room Name, Amount Paid, Payment Status */}
-            <div className='w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-80 overflow-y-scroll mt-3'>
+            <div className='w-full text-left border border-gray-300 rounded-lg overflow-y-scroll mt-3'>
                 <table className='w-full' >
                     <thead className='bg-gray-50 '>
                         <tr>
                             <th className='py-3 px-4 text-gray-800 font-medium'>Name</th>
-                            <th className='py-3 px-4 text-gray-800 font-medium max-sm:hidden'>Facility</th>
-                            <th className='py-3 px-4 text-gray-800 font-medium'>Price / night</th>
+                            <th className='py-3 px-4 text-gray-800 font-medium max-sm:hidden'>Address</th>
+                            <th className='py-3 px-4 text-gray-800 font-medium'>Contact</th>
+                            <th className='py-3 px-4 text-gray-800 font-medium text-center'>Owner</th>
+                            <th className='py-3 px-4 text-gray-800 font-medium text-center'>Location</th>
                             <th className='py-3 px-4 text-gray-800 font-medium text-center'>Actions</th>
                         </tr>
                     </thead>
