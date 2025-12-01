8e2ab755a48f2b0a2c17cf3804d9081cf78f19f9
Samuel Udobong <samuelleumasudo@gmail.com>
2025-12-01 07:38:21 +0100

    update

--- client/src/pages/hotelOwner/Layout.jsx
+++ client/src/pages/hotelOwner/Layout.jsx
@@ -1,30 +1,22 @@
-import React, { useEffect } from 'react'
-import Navbar from '../../components/hotelOwner/Navbar'
-import Sidebar from '../../components/hotelOwner/Sidebar'
-import { Outlet } from 'react-router-dom'
-import { useAppContext } from '../../context/AppContext'
-
 const Layout = () => {
-
     const { isOwner, navigate } = useAppContext()
 
     useEffect(() => {
-        if (!isOwner) {
-            navigate('/')
-        }
+        if (!isOwner) navigate('/')
     }, [isOwner])
 
     return (
-        <div className='flex flex-col h-screen'>
+        <div className='flex flex-col min-h-screen'>
             <Navbar />
-            <div className='flex h-full'>
+
+            <div className='flex flex-1 overflow-hidden'>
                 <Sidebar />
-                <div className='flex-1 p-4 pt-10 md:px-10 h-full'>
+
+                {/* main content */}
+                <div className='flex-1 p-4 pt-16 md:px-10 overflow-y-auto'>
                     <Outlet />
                 </div>
             </div>
         </div>
     )
 }
-
-export default Layout
