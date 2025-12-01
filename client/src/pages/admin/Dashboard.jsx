130950502ecee947c5ab9a9d5d5ea55bbee53907
Samuel Udobong <samuelleumasudo@gmail.com>
2025-12-01 01:38:56 +0100

    update

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useAppContext } from "../../context/AppContext";
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 import { BadgeCheck, XCircle, Clock } from "lucide-react";
