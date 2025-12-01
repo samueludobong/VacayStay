95618813cc6f7120f21ff29eb26d756ae2822cef
Samuel Udobong <samuelleumasudo@gmail.com>
2025-12-01 02:34:48 +0100

    update

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg'
+import logo from './vacaystay.svg'
 import searchIcon from './searchIcon.svg'
 import userIcon from './userIcon.svg'
 import calenderIcon from './calenderIcon.svg'
