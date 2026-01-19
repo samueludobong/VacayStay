a9ca08c64952d0ce37cc2ce1f1e6fffd493eaf5f
Samuel Udobong <samuelleumasudo@gmail.com>
2026-01-19 15:16:54 +0100

    update

--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -14,7 +14,7 @@ const FeaturedDestination = () => {
                     <HotelCard key={room._id} room={room} index={index} />
                 ))}
             </div>
-            <button onClick={() => { navigate('/rooms'); scrollTo(0, 0) }} className='my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer'>
+            <button onClick={() => { navigate('/hotels'); scrollTo(0, 0) }} className='my-16 px-4 py-2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursor-pointer'>
                 View All Destinations
             </button>
         </div>
