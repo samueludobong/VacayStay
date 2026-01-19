301bdb951d1e883b352f9dd68f15e7c63eabfb7d
Samuel Udobong <samuelleumasudo@gmail.com>
2026-01-19 15:14:36 +0100

    update

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -40,8 +40,8 @@ const Hero = () => {
                 <button
                     class="bg-[#396ce5] text-white px-6 py-2.5 h-14 rounded-sm text-sm font-medium flex items-center text-center space-x-2 hover:bg-[#3360CC] transition"
                     type="button"
-                    onClick={() => navigate('/rooms')}>
-                        Visit Rooms
+                    onClick={() => navigate('/hotels')}>
+                        Visit Hotels
                 </button>
                 <a class="bg-white text-[#396ce5] px-6 py-2.5 h-14 rounded-sm text-sm font-medium flex items-center text-center space-x-2 hover:bg-[#F7F7F7] transition"
                     href="#">
