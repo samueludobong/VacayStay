a62b8755cea46ec0f0b45bc0167a1102d385ce17
Samuel Udobong <samuelleumasudo@gmail.com>
2026-01-20 01:07:54 +0100

    update

--- server/controllers/newsletterController.js
+++ server/controllers/newsletterController.js
@@ -1,8 +1,8 @@
 import Newsletter from "../models/NewsLetter.js";
 import { transporter } from "../configs/node.js";
 
-app.post("/api/newsletter/subscribe", async (req, res) => {
-  const { email } = req.body;
+export const Subscribe = async (req, res) => {
+   const { email } = req.body;
 
   if (!email) {
     return res.status(400).json({ message: "Email is required" });
@@ -34,5 +34,6 @@ app.post("/api/newsletter/subscribe", async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server error" });
-  }
-});
+  }};
+  
+export default Subscribe;
--- server/routes/newsletterRoutes.js
+++ server/routes/newsletterRoutes.js
@@ -1,1 +1,10 @@
-im
+import express from "express";
+import Subscribe from "../controllers/newsletterController.js";
+
+
+const newsletterRouter = express.Router();
+
+newsletterRouter.post("/subscribe", Subscribe);
+
+
+export default newsletterRouter;
