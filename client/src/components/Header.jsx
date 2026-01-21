c9b0f82978320da718f79642aa3ea6a1ec3ba1f5
Samuel Udobong <samuelleumasudo@gmail.com>
2026-01-21 02:00:11 +0100

    update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -15,11 +15,11 @@ export default function Header() {
             <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
                 <a href="#contact"
                     className="px-10 py-2.5 border rounded-full bg-gradient-to-r from-[#b820e6] to-[#da7d20] text-white flex items-center gap-2 dark:border-transparent">
-                    contact me <img src={assets.logo} alt="" className="w-4" />
+                    Contact Us <img src={assets.logo} alt="" className="w-4" />
                 </a>
 
                 <a href="./assets/dev-icon.png" download
-                    className="px-10 py-2.5 rounded-full border border-gray-300 dark:border-white/25 hover:bg-slate-100/70 dark:hover:bg-darkHover flex items-center gap-2 bg-white dark:bg-transparent dark:text-white">
+                    className="px-10 py-2.5 rounded-full border border-white dark:border-white hover:bg-slate-100/70 dark:hover:bg-darkHover flex items-center gap-2 bg-white dark:bg-transparent dark:text-black">
                     my resume <img src="./assets/download-icon.png" alt="" className="w-4 dark:invert" />
                 </a>
             </div>
