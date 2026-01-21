ff82335df915edb662fc2494c920a49a44135b0d
Samuel Udobong <samuelleumasudo@gmail.com>
2026-01-21 14:14:30 +0100

    update

--- client/src/pages/ContactUS.jsx
+++ client/src/pages/ContactUS.jsx
@@ -13,7 +13,6 @@ export default function ContactUS() {
             <Navbar />
             <Header />
             <Contact />
-            <Footer />
         </>
     )
 }
--- client/src/pages/Experience.jsx
+++ client/src/pages/Experience.jsx
@@ -16,7 +16,6 @@ export default function Experience() {
             <About />
             <Services />
             <Contact />
-            <Footer />
         </>
     )
 }
