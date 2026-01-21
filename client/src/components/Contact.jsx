96707f075dd2ef123c96a338d5085f66d4b352a6
Samuel Udobong <samuelleumasudo@gmail.com>
2026-01-21 14:35:59 +0100

    update

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -29,45 +29,6 @@ export default function Contact() {
   }
 };
 
-
-    function CaptchaLoader() {
-        const captchadiv = document.querySelectorAll('[data-captcha="true"]');
-        if (captchadiv.length) {
-            let lang = null;
-            let onload = null;
-            let render = null;
-
-            captchadiv.forEach(function (item) {
-                const sitekey = item.dataset.sitekey;
-                lang = item.dataset.lang;
-                onload = item.dataset.onload;
-                render = item.dataset.render;
-
-                if (!sitekey) {
-                    item.dataset.sitekey = "50b2fe65-b00b-4b9e-ad62-3ba471098be2";
-                }
-            });
-
-            let scriptSrc = "https://js.hcaptcha.com/1/api.js?recaptchacompat=off";
-            if (lang) {
-                scriptSrc += `&hl=${lang}`;
-            }
-            if (onload) {
-                scriptSrc += `&onload=${onload}`;
-            }
-            if (render) {
-                scriptSrc += `&render=${render}`;
-            }
-
-            var script = document.createElement("script");
-            script.type = "text/javascript";
-            script.async = true;
-            script.defer = true;
-            script.src = scriptSrc;
-            document.body.appendChild(script);
-        }
-    }
-
     return (
         <div id="contact" className="w-full px-[12%] py-10 scroll-mt-20 bg-[url('./assets/footer-bg-color.png')] bg-no-repeat bg-[length:90%_auto] bg-center dark:bg-none">
 
--- client/src/components/ContactUSEro.jsx
+++ client/src/components/ContactUSEro.jsx
@@ -7,36 +7,36 @@ const ContactUSEro = () => {
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
-
+    console.log("Form submitted with input:", input);
+      
 
   }
 
   return (
     <>
-      <section className="flex flex-col items-center text-white text-sm pb-20 px-4 font-poppins">
-          {/* BACKGROUND IMAGE */}
+      <section className="flex flex-col items-center text-white text-sm pb-20 px-4 font-poppins bg-[#0a1b43]">
           <img src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/refs/heads/main/assets/hero/bg-gradient-2.png" className="absolute inset-0 -z-10 size-full opacity" alt="" />
 
-        <a href="https://prebuiltui.com" className="flex items-center gap-2 border border-slate-700 rounded-full p-1 pr-3 text-sm mt-20">
-          <span className="bg-indigo-600 text-xs px-3 py-1 rounded-full">NEW</span>
+        <a href="#" className="flex items-center gap-2 border border-slate-700 rounded-full p-1 pr-3 text-sm mt-20">
+          <span className="bg-indigo-600 text-xs px-3 py-1 rounded-full">Online</span>
           <p className="flex items-center gap-2">
-            <span>Try 30 days free trial option</span>
+            <span>At your service 24/7</span>
             <svg className="mt-px" width="6" height="9" viewBox="0 0 6 9" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="m1 1 4 3.5L1 8" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/></svg>
           </p>
         </a>
 
         <h1 className="text-center text-[40px] leading-[48px] md:text-6xl md:leading-[70px] mt-4 font-semibold max-w-3xl">
-          Turn thoughts into slides instantly, with AI.
+          Contact Us for Exceptional Support and Assistance
         </h1>
 
         <p className="text-center text-base max-w-md mt-2">
-          Create, customize and present faster than ever with intelligent design powered by AI.
+          Our dedicated team is here to help you with any inquiries or issues you may have. Reach out to us and experience top-notch customer service.
         </p>
 
         <form onSubmit={onSubmitHandler} className="bg-white/10 max-w-2xl w-full rounded-xl p-4 mt-10 border border-indigo-600/70 focus-within:ring-2 ring-indigo-500 transition-all">
-          <textarea onChange={e => setInput(e.target.value)} className="bg-transparent outline-none text-gray-300 resize-none w-full" rows={4} placeholder="Describe your presentation in details" required />
+          <textarea onChange={e => setInput(e.target.value)} className="bg-transparent outline-none text-gray-300 resize-none w-full" rows={4} placeholder="Describe your inquiry, use for below for detailed, feedback, and possible feedback" required />
           <button className="ml-auto flex items-center gap-2 bg-gradient-to-r from-[#CB52D4] to-indigo-600 rounded-md px-4 py-2">
-            Create with AI
+            <span>Get in Touch</span>
           </button>
         </form>
 
