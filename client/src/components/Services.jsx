fbfb00311fed7db639431ef901148e3cb6fa7e18
Samuel Udobong <samuelleumasudo@gmail.com>
2026-01-27 16:01:11 +0100

    update

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -29,7 +29,7 @@ export default function Services() {
         <div id="services" className="w-full px-[12%] py-10 scroll-mt-20">
             <h4 className="text-center mb-2 text-lg font-Ovo">What we offer</h4>
             <h2 className="text-center text-5xl font-Ovo">Our services</h2>
-            <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.</p>
+            <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">We provide a comprehensive suite of services to help you manage your hotel business efficiently and effectively.</p>
 
             <div className="grid grid-cols-auto gap-6 my-10">
                 {services.map((service) => (
